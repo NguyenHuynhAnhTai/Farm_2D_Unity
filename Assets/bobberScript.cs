03e73ff6dfefbcd47cba240e6ea0857f6a2a6ee7
Đinh Văn Phong <phongdvse171245@fpt.edu.vn>
2024-06-25 01:09:02 +0700

    Fishing Scene

---
+++ Assets/bobberScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bobberScript : MonoBehaviour
+{
+    public bool gameIsOver;
+    public Animator bobberAnim;
+    public float bobberTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameIsOver = false;
+        bobberTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bobberTime += Time.deltaTime;
+        if (bobberTime >= 3)
+        {
+            bobberAnim.Play("booberFish");
+        }
+        if (Input.GetKeyDown(KeyCode.P) && bobberTime <= 3)
+        {
+            Destroy(gameObject);
+        }
+        if (gameIsOver == true) 
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    public void gameOver()
+    {
+        gameIsOver = true;
+    }
+}
